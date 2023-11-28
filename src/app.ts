62e15abb874ddd2878dd2cf3420cfa42e3f9f66b
Johannes Oster <johannes.oster@outlook.at>
2023-11-28 17:42:51 +0100

    Add links to network graph
    Connected nodes now have a visual non-directed connection between them
    that automatically follows them on drag.

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,46 @@
 import * as d3 from "d3";
 
 interface Circle {
+  id: number;
   cx: number;
   cy: number;
   radius: number;
   color: string;
 }
 
-const container = d3.select("#container");
+const container = d3.select("#d3-container");
 const svg = container
   .append("svg")
   .attr("width", "100%")
   .attr("height", "100%");
 
 const circlesData: Circle[] = [
-  { cx: 200, cy: 200, radius: 30, color: "green" },
-  { cx: 100, cy: 100, radius: 40, color: "blue" },
+  { id: 0, cx: 200, cy: 200, radius: 30, color: "navy" },
+  { id: 1, cx: 100, cy: 100, radius: 40, color: "black" },
+  { id: 2, cx: 130, cy: 180, radius: 20, color: "darkcyan" },
+];
+
+const linksData = [
+  { source: circlesData[0], target: circlesData[1] },
+  { source: circlesData[1], target: circlesData[2] },
 ];
 
 const g = svg.append("g");
 
 let offsetX = 0,
   offsetY = 0;
+
+const links = g
+  .selectAll("line")
+  .data(linksData)
+  .enter()
+  .append("line")
+  .attr("stroke", "black")
+  .attr("stroke-width", 2)
+  .attr("x1", (d) => d.source.cx)
+  .attr("y1", (d) => d.source.cy)
+  .attr("x2", (d) => d.target.cx)
+  .attr("y2", (d) => d.target.cy);
 
 g.selectAll("circle")
   .data(circlesData)
@@ -40,10 +59,19 @@ g.selectAll("circle")
         offsetY = parseFloat(node.attr("cy")) - event.y;
         d3.select(this).raise().classed("active", true);
       })
-      .on("drag", function (event) {
+      .on("drag", function (event, d) {
         d3.select(this)
           .attr("cx", event.x + offsetX)
           .attr("cy", event.y + offsetY);
+
+        links
+          .filter((l) => l.source === d)
+          .attr("x1", event.x + offsetX)
+          .attr("y1", event.y + offsetY);
+        links
+          .filter((l) => l.target === d)
+          .attr("x2", event.x + offsetX)
+          .attr("y2", event.y + offsetY);
       })
   );
 
