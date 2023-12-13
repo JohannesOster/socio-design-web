03ad2e0cec6a401352750e64de8dcf23cf476aac
Johannes Oster <johannes.oster@outlook.at>
2023-12-13 13:35:23 +0100

    Updates

--- index.301580f7.js
+++ index.301580f7.js
@@ -741,7 +741,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
             {
                 selector: "node",
                 style: {
-                    label: "data(id)",
+                    label: "data(id)"
+                }
+            },
+            {
+                selector: "node.highlight",
+                style: {
                     "background-color": "#273541"
                 }
             },
@@ -749,9 +754,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 selector: "edge",
                 style: {
                     width: 2,
-                    "line-color": "#273541",
-                    "source-arrow-color": "#273541",
-                    "target-arrow-color": "#273541",
                     "curve-style": "straight",
                     "target-arrow-shape": "triangle"
                 }
@@ -831,6 +833,161 @@ document.addEventListener("DOMContentLoaded", ()=>{
         })).sort((a, b)=>a.pr < b.pr ? 1 : -1);
     console.log("========== Page Rank | Concept: TBD");
     console.log(pageRanks);
+    /**================================== */ const container = document.getElementById("cmd-palette");
+    // Track mouse movement to be able to add new vertex where mouse is placed
+    let mousePos = {
+        x: 0,
+        y: 0
+    };
+    document.body.addEventListener("mousemove", (event)=>{
+        mousePos = {
+            x: event.clientX,
+            y: event.clientY
+        };
+    });
+    const convertToCytoscapeCoordinates = (mousePos)=>{
+        const pan = cy.pan();
+        const zoom = cy.zoom();
+        return {
+            x: (mousePos.x - pan.x) / zoom,
+            y: (mousePos.y - pan.y) / zoom
+        };
+    };
+    document.addEventListener("keydown", (event)=>{
+        if (!container) return;
+        if (event.key === "a" && event.metaKey) {
+            // Open cmd-palette on cmd+a
+            container.classList.toggle("hidden", false); // Remove 'hidden' class to show the input
+            container.classList.toggle("block", true); // Add 'block' class
+            document.getElementById("cmd-palette-input")?.focus();
+        }
+        if (event.key === "Escape") // Close cmd-palette on escape
+        {
+            if (!container.classList.contains("hidden")) {
+                container.classList.add("hidden");
+                container.classList.remove("block");
+            }
+        }
+        if (event.key === "Enter" && !container.classList.contains("hidden")) {
+            container.classList.add("hidden");
+            container.classList.remove("block");
+            const input = document.getElementById("cmd-palette-input");
+            if (!input) return;
+            const value = input.value;
+            const cyPos = convertToCytoscapeCoordinates(mousePos);
+            cy.add({
+                group: "nodes",
+                data: {
+                    id: value
+                },
+                position: cyPos
+            });
+        }
+    });
+    document.addEventListener("click", (event)=>{
+        // Close cmd-palette on outside click
+        const input = document.getElementById("cmd-palette-input");
+        if (!(container && input)) return;
+        if (!input.contains(event.target) && !container.classList.contains("hidden")) {
+            container.classList.add("hidden");
+            container.classList.remove("block");
+        }
+    });
+    /** DRAWING ============================= */ let tempEdge = null;
+    let isDrawing = false;
+    let sourceNode = null;
+    let ghostNode = null;
+    let targetNode = null;
+    let highlightedNode = null;
+    cy.on("mousedown", "node", (event)=>{
+        if (event.originalEvent.shiftKey) {
+            isDrawing = true;
+            sourceNode = event.target;
+            sourceNode.ungrabify();
+            ghostNode = cy.add({
+                group: "nodes",
+                data: {
+                    id: "ghost-node"
+                },
+                position: event.position,
+                classes: [
+                    "ghost-node"
+                ],
+                style: {
+                    width: 1,
+                    height: 1,
+                    opacity: 0
+                }
+            });
+            tempEdge = cy.add({
+                group: "edges",
+                data: {
+                    id: "temp-edge",
+                    source: sourceNode.id(),
+                    target: ghostNode.id()
+                },
+                style: {
+                    "line-style": "dashed"
+                },
+                classes: [
+                    "temporary-edge"
+                ]
+            });
+            cy.boxSelectionEnabled(false);
+        }
+    });
+    cy.on("mousemove", (event)=>{
+        const THRESHHOLD = 30;
+        const isClose = (node1, node2)=>{
+            let p1 = node1.position();
+            let p2 = node2.position();
+            const distance = Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+            return distance < THRESHHOLD;
+        };
+        if (isDrawing && tempEdge) {
+            ghostNode.position(event.position);
+            let closestNode = cy.nodes().filter((node)=>{
+                if (node.id() === sourceNode.id()) return false;
+                if (node.id() === ghostNode.id()) return false;
+                if (isClose(node, ghostNode)) {
+                    if (highlightedNode && highlightedNode.id()) {
+                        if (highlightedNode.id() === node.id()) return true;
+                    }
+                    highlightedNode = node;
+                    node.addClass("highlight");
+                    return true;
+                } else node.removeClass("highlight");
+                return false;
+            }).first();
+            if (closestNode.id()) targetNode = closestNode;
+            else {
+                highlightedNode = null;
+                targetNode = ghostNode;
+            }
+        }
+    });
+    cy.on("mouseup", (event)=>{
+        if (isDrawing && tempEdge) {
+            if (targetNode !== cy && targetNode.isNode()) // Create permanent edge
+            cy.add({
+                group: "edges",
+                data: {
+                    source: sourceNode.id(),
+                    target: targetNode.id()
+                }
+            });
+            cy.remove(ghostNode);
+            sourceNode.grabify();
+            cy.boxSelectionEnabled(true);
+            // Remove temporary edge
+            tempEdge.remove();
+            tempEdge = null;
+            isDrawing = false;
+            sourceNode = null;
+            highlightedNode?.removeClass("highlight");
+            highlightedNode = null;
+        }
+    });
 });
 
 },{"cytoscape":"cxe8j","cytoscape-cola":"9EsJJ","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"cxe8j":[function(require,module,exports) {
