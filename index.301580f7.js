2a05953e679eccb5b7a7cdc86aa573a8ab273146
Johannes Oster <johannes.oster@outlook.at>
2023-12-18 11:51:11 +0100

    Updates

--- index.301580f7.js
+++ index.301580f7.js
@@ -583,6 +583,13 @@ var _cytoscapeColaDefault = parcelHelpers.interopDefault(_cytoscapeCola);
 var _defaultGraph = require("./defaultGraph");
 (0, _cytoscapeDefault.default).use((0, _cytoscapeColaDefault.default));
 document.addEventListener("DOMContentLoaded", ()=>{
+    const cy = initCytoscape();
+    setupCommandPalette(cy);
+    setupEdgeDrawingHandler(cy);
+    setUpOptimizer(cy);
+    setUpAnalyzerr(cy);
+});
+const initCytoscape = ()=>{
     const cy = (0, _cytoscapeDefault.default)({
         container: document.getElementById("cy-container"),
         minZoom: 0.25,
@@ -614,7 +621,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
             }
         ]
     });
-    /** Add Nodes ============================= */ const container = document.getElementById("cmd-palette");
+    return cy;
+};
+const setupCommandPalette = (cy)=>{
+    const CMD_PALETTE_CONTAINER_ID = "cmd-palette";
+    const container = document.getElementById(CMD_PALETTE_CONTAINER_ID);
+    if (!container) throw new Error(`Missing cmd palette container with id: ${CMD_PALETTE_CONTAINER_ID}`);
     // Track mouse movement to be able to add new vertex where mouse is placed
     let mousePos = {
         x: 0,
@@ -634,23 +646,32 @@ document.addEventListener("DOMContentLoaded", ()=>{
             y: (mousePos.y - pan.y) / zoom
         };
     };
-    let selectedNode = null;
-    document.addEventListener("keydown", (event)=>{
-        if (!container) return;
-        if (event.key === "a" && (event.metaKey || event.ctrlKey)) {
+    const cmdMap = {
+        "cmd+a": ()=>{
             // Open cmd-palette on cmd+a
             container.classList.toggle("hidden", false); // Remove 'hidden' class to show the input
             container.classList.toggle("block", true); // Add 'block' class
             document.getElementById("cmd-palette-input")?.focus();
-        }
-        if (event.key === "Escape") // Close cmd-palette on escape
-        {
-            if (!container.classList.contains("hidden")) {
-                container.classList.add("hidden");
-                container.classList.remove("block");
-            }
-        }
-        if (event.key === "Enter" && !container.classList.contains("hidden")) {
+        },
+        Escape: ()=>{
+            // Close cmd-palette on escape
+            if (container.classList.contains("hidden")) return;
+            container.classList.add("hidden");
+            container.classList.remove("block");
+        },
+        del: ()=>{
+            const elemensToDelete = cy.elements("node:selected");
+            elemensToDelete.forEach((selectedNode)=>{
+                cy.remove(selectedNode.connectedEdges());
+                cy.remove(selectedNode);
+            });
+            const edgesToDelete = cy.elements("edge:selected");
+            edgesToDelete.forEach((selectedEdge)=>{
+                cy.remove(selectedEdge);
+            });
+        },
+        Enter: ()=>{
+            if (container.classList.contains("hidden")) return;
             container.classList.add("hidden");
             container.classList.remove("block");
             const input = document.getElementById("cmd-palette-input");
@@ -664,23 +685,29 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 },
                 position: cyPos
             });
-        }
-        // - Save
-        if (event.key === "s" && (event.metaKey || event.ctrlKey)) {
+        },
+        "cmd+s": (event)=>{
             event.preventDefault();
-            saveGraph();
+            const elements = cy.json().elements; // Get the current state of the graph
+            saveGraph(elements);
         }
-        if (event.key === "Del" || event.key === "Delete" || event.key === "Backspace") {
-            const elemensToDelete = cy.elements("node:selected");
-            elemensToDelete.forEach((selectedNode)=>{
-                cy.remove(selectedNode.connectedEdges());
-                cy.remove(selectedNode);
-            });
-            const edgesToDelete = cy.elements("edge:selected");
-            edgesToDelete.forEach((selectedEdge)=>{
-                cy.remove(selectedEdge);
-            });
-        }
+    };
+    const constructCmdKey = ({ key, metaKey, ctrlKey })=>{
+        let cmd = "";
+        if (metaKey || ctrlKey) cmd += "cmd+";
+        cmd += key;
+        // unify delete
+        cmd = cmd.replace("Del", "del");
+        cmd = cmd.replace("Delete", "del");
+        cmd = cmd.replace("Backspace", "del");
+        // remove "Meta" when cmd only is pressed
+        cmd = cmd.replace("Meta", "");
+        return cmd;
+    };
+    document.addEventListener("keydown", (event)=>{
+        const cmdKey = constructCmdKey(event);
+        const cmd = cmdMap[cmdKey];
+        if (cmd) cmd(event);
     });
     document.addEventListener("click", (event)=>{
         // Close cmd-palette on outside click
@@ -691,6 +718,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
             container.classList.remove("block");
         }
     });
+};
+const setupEdgeDrawingHandler = (cy)=>{
     /** Draw edges ============================= */ let tempEdge = null;
     let isDrawing = false;
     let sourceNode = null;
@@ -786,12 +815,49 @@ document.addEventListener("DOMContentLoaded", ()=>{
             highlightedNode = null;
         }
     });
-    /** Trigger Optimizat ============================= */ const triggerBtn = document.getElementById("triggerBtn");
+};
+const setUpOptimizer = (cy)=>{
+    /** Trigger Optimization ============================= */ const triggerBtn = document.getElementById("optimizeBtn");
     if (triggerBtn) triggerBtn.onclick = (event)=>{
         cy.layout({
             name: "cola",
             animate: false
         }).run();
+    };
+};
+const setUpAnalyzerr = (cy)=>{
+    /** Trigger Optimization ============================= */ const triggerBtn = document.getElementById("analyzeBtn");
+    // ========= Toggle Metric Containers
+    const degreeCentralityContainer = document.getElementById("degree-centrality");
+    const degreeCentralityBody = degreeCentralityContainer.getElementsByTagName("ul")[0];
+    degreeCentralityContainer.onclick = ()=>{
+        degreeCentralityBody.hidden = !degreeCentralityBody.hidden;
+    };
+    const degreeCentralityNormalizedContainer = document.getElementById("degree-centrality-normalized");
+    const degreeCentralityNormalizedBody = degreeCentralityNormalizedContainer.getElementsByTagName("ul")[0];
+    degreeCentralityNormalizedContainer.onclick = ()=>{
+        degreeCentralityNormalizedBody.hidden = !degreeCentralityNormalizedBody.hidden;
+    };
+    const betweennessCentralityNormalizedContainer = document.getElementById("betweenness-centrality-normalized");
+    const betweennessCentralityNormalizedBody = betweennessCentralityNormalizedContainer.getElementsByTagName("ul")[0];
+    betweennessCentralityNormalizedContainer.onclick = ()=>{
+        betweennessCentralityNormalizedBody.hidden = !betweennessCentralityNormalizedBody.hidden;
+    };
+    const closenessCentralityNormalizedContainer = document.getElementById("closeness-centrality-normalized");
+    const closenessCentralityNormalizedBody = closenessCentralityNormalizedContainer.getElementsByTagName("ul")[0];
+    closenessCentralityNormalizedContainer.onclick = ()=>{
+        closenessCentralityNormalizedBody.hidden = !closenessCentralityNormalizedBody.hidden;
+    };
+    const pageRankContainer = document.getElementById("page-rank");
+    const pageRankBody = pageRankContainer.getElementsByTagName("ul")[0];
+    pageRankContainer.onclick = ()=>{
+        pageRankBody.hidden = !pageRankBody.hidden;
+    };
+    const round = (value, decimals)=>{
+        const shifted = value * Math.pow(10, decimals);
+        return Number((Math.round(shifted) / Math.pow(10, decimals)).toFixed(decimals));
+    };
+    if (triggerBtn) triggerBtn.onclick = ()=>{
         const nodes = cy.nodes();
         const selector = cy.$();
         // ========== Degreee Centrality | Concept: TBD.
@@ -807,6 +873,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 in: dc.indegree,
                 out: dc.outdegree
             };
+        }).sort((a, b)=>a.id > b.id ? 1 : -1);
+        degreeCentralityBody.innerHTML = "";
+        degreeCentrality.forEach((item)=>{
+            const li = document.createElement("li"); // Create a new <li> element
+            li.textContent = JSON.stringify(item); // Set its text content
+            degreeCentralityBody.appendChild(li); // Append it to the <ul>
         });
         console.log("========== Degreee Centrality | Concept: Connectivity (In & Out)");
         console.log(degreeCentrality);
@@ -816,9 +888,15 @@ document.addEventListener("DOMContentLoaded", ()=>{
             });
             return {
                 id: node.id(),
-                in: dc.indegree(node),
-                out: dc.outdegree(node)
+                in: round(dc.indegree(node), 3),
+                out: round(dc.outdegree(node), 3)
             };
+        }).sort((a, b)=>a.id > b.id ? 1 : -1);
+        degreeCentralityNormalizedBody.innerHTML = "";
+        degreeCentralityNormalized.forEach((item)=>{
+            const li = document.createElement("li"); // Create a new <li> element
+            li.textContent = JSON.stringify(item); // Set its text content
+            degreeCentralityNormalizedBody.appendChild(li); // Append it to the <ul>
         });
         console.log("========== Normalized Degreee Centrality | Concept: Relative connectivity (In & Out).");
         console.log(degreeCentralityNormalized);
@@ -833,8 +911,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
        */ const bc = selector.bc();
         const betweenness = nodes.map((node)=>({
                 id: node.id(),
-                bc: bc.betweennessNormalized(node)
+                bc: round(bc.betweennessNormalized(node), 3)
             })).sort((a, b)=>a.bc < b.bc ? 1 : -1);
+        betweennessCentralityNormalizedBody.innerHTML = "";
+        betweenness.forEach((item)=>{
+            const li = document.createElement("li"); // Create a new <li> element
+            li.textContent = JSON.stringify(item); // Set its text content
+            betweennessCentralityNormalizedBody.appendChild(li); // Append it to the <ul>
+        });
         console.log("========== Normalized Betweenness Centrality | Concept: Gatekeepers");
         console.log(betweenness);
         // ========== Normalized Closeness Centrality | Concept: Central / excluded figures
@@ -846,8 +930,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
        */ const ccn = selector.ccn();
         const closenessCentrality = nodes.map((node)=>({
                 id: node.id(),
-                cc: ccn.closeness(node)
+                cc: round(ccn.closeness(node), 3)
             })).sort((a, b)=>a.cc < b.cc ? 1 : -1);
+        closenessCentralityNormalizedBody.innerHTML = "";
+        closenessCentrality.forEach((item)=>{
+            const li = document.createElement("li"); // Create a new <li> element
+            li.textContent = JSON.stringify(item); // Set its text content
+            closenessCentralityNormalizedBody.appendChild(li); // Append it to the <ul>
+        });
         console.log("========== Normalized Closeness Centrality | Concept: Central / excluded figures");
         console.log(closenessCentrality);
         // ========== Normalized Page Rank | Concept: Relative Importance
@@ -857,25 +947,29 @@ document.addEventListener("DOMContentLoaded", ()=>{
        */ const pr = cy.elements().pageRank();
         const pageRanks = nodes.map((node)=>({
                 id: node.id(),
-                pr: pr.rank(node)
+                pr: round(pr.rank(node), 3)
             })).sort((a, b)=>a.pr < b.pr ? 1 : -1);
+        pageRankBody.innerHTML = "";
+        pageRanks.forEach((item)=>{
+            const li = document.createElement("li"); // Create a new <li> element
+            li.textContent = JSON.stringify(item); // Set its text content
+            pageRankBody.appendChild(li); // Append it to the <ul>
+        });
         console.log("========== Page Rank | Concept: TBD");
         console.log(pageRanks);
+        cy.resize();
     };
-    /** SAVING ====================================  */ function saveGraph() {
-        console.log("saved");
-        const elements = cy.json().elements; // Get the current state of the graph
-        localStorage.setItem("cyGraph", JSON.stringify(elements)); // Save it as a string
-    }
-    function loadGraph() {
-        const savedData = localStorage.getItem("cyGraph");
-        if (savedData) {
-            const json = JSON.parse(savedData); // Parse it back to JSON
-            return Object.keys(json).length ? json : null;
-        }
-        return null; // or return a default state if nothing is saved
-    }
-});
+};
+const saveGraph = (elements)=>{
+    localStorage.setItem("cyGraph", JSON.stringify(elements));
+    console.log("Succesfully saved graph!");
+};
+const loadGraph = ()=>{
+    const savedData = localStorage.getItem("cyGraph");
+    if (!savedData) return null;
+    const json = JSON.parse(savedData);
+    return Object.keys(json).length ? json : null;
+};
 
 },{"cytoscape":"cxe8j","cytoscape-cola":"9EsJJ","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","./defaultGraph":"4nL1s"}],"cxe8j":[function(require,module,exports) {
 /**
--- index.f6653313.js
+++ index.f6653313.js
@@ -32921,11 +32921,18 @@ const $0028bee6a66174ce$export$d3dde9fdaf372e40 = $0028bee6a66174ce$var$vertices
 
 (0, (/*@__PURE__*/$parcel$interopDefault($a953f3bfbe7f4a7f$exports))).use((0, (/*@__PURE__*/$parcel$interopDefault($49e5a9b6df5322f4$exports))));
 document.addEventListener("DOMContentLoaded", ()=>{
+    const cy = $915c7ff9e95902f7$var$initCytoscape();
+    $915c7ff9e95902f7$var$setupCommandPalette(cy);
+    $915c7ff9e95902f7$var$setupEdgeDrawingHandler(cy);
+    $915c7ff9e95902f7$var$setUpOptimizer(cy);
+    $915c7ff9e95902f7$var$setUpAnalyzerr(cy);
+});
+const $915c7ff9e95902f7$var$initCytoscape = ()=>{
     const cy = (0, (/*@__PURE__*/$parcel$interopDefault($a953f3bfbe7f4a7f$exports)))({
         container: document.getElementById("cy-container"),
         minZoom: 0.25,
         maxZoom: 8,
-        elements: loadGraph() || (0, $0028bee6a66174ce$export$d3dde9fdaf372e40),
+        elements: $915c7ff9e95902f7$var$loadGraph() || (0, $0028bee6a66174ce$export$d3dde9fdaf372e40),
         layout: {
             name: "preset"
         },
@@ -32952,7 +32959,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
             }
         ]
     });
-    /** Add Nodes ============================= */ const container = document.getElementById("cmd-palette");
+    return cy;
+};
+const $915c7ff9e95902f7$var$setupCommandPalette = (cy)=>{
+    const CMD_PALETTE_CONTAINER_ID = "cmd-palette";
+    const container = document.getElementById(CMD_PALETTE_CONTAINER_ID);
+    if (!container) throw new Error(`Missing cmd palette container with id: ${CMD_PALETTE_CONTAINER_ID}`);
     // Track mouse movement to be able to add new vertex where mouse is placed
     let mousePos = {
         x: 0,
@@ -32972,23 +32984,32 @@ document.addEventListener("DOMContentLoaded", ()=>{
             y: (mousePos.y - pan.y) / zoom
         };
     };
-    let selectedNode = null;
-    document.addEventListener("keydown", (event)=>{
-        if (!container) return;
-        if (event.key === "a" && (event.metaKey || event.ctrlKey)) {
+    const cmdMap = {
+        "cmd+a": ()=>{
             // Open cmd-palette on cmd+a
             container.classList.toggle("hidden", false); // Remove 'hidden' class to show the input
             container.classList.toggle("block", true); // Add 'block' class
             document.getElementById("cmd-palette-input")?.focus();
-        }
-        if (event.key === "Escape") // Close cmd-palette on escape
-        {
-            if (!container.classList.contains("hidden")) {
-                container.classList.add("hidden");
-                container.classList.remove("block");
-            }
-        }
-        if (event.key === "Enter" && !container.classList.contains("hidden")) {
+        },
+        Escape: ()=>{
+            // Close cmd-palette on escape
+            if (container.classList.contains("hidden")) return;
+            container.classList.add("hidden");
+            container.classList.remove("block");
+        },
+        del: ()=>{
+            const elemensToDelete = cy.elements("node:selected");
+            elemensToDelete.forEach((selectedNode)=>{
+                cy.remove(selectedNode.connectedEdges());
+                cy.remove(selectedNode);
+            });
+            const edgesToDelete = cy.elements("edge:selected");
+            edgesToDelete.forEach((selectedEdge)=>{
+                cy.remove(selectedEdge);
+            });
+        },
+        Enter: ()=>{
+            if (container.classList.contains("hidden")) return;
             container.classList.add("hidden");
             container.classList.remove("block");
             const input = document.getElementById("cmd-palette-input");
@@ -33002,23 +33023,29 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 },
                 position: cyPos
             });
-        }
-        // - Save
-        if (event.key === "s" && (event.metaKey || event.ctrlKey)) {
+        },
+        "cmd+s": (event)=>{
             event.preventDefault();
-            saveGraph();
+            const elements = cy.json().elements; // Get the current state of the graph
+            $915c7ff9e95902f7$var$saveGraph(elements);
         }
-        if (event.key === "Del" || event.key === "Delete" || event.key === "Backspace") {
-            const elemensToDelete = cy.elements("node:selected");
-            elemensToDelete.forEach((selectedNode)=>{
-                cy.remove(selectedNode.connectedEdges());
-                cy.remove(selectedNode);
-            });
-            const edgesToDelete = cy.elements("edge:selected");
-            edgesToDelete.forEach((selectedEdge)=>{
-                cy.remove(selectedEdge);
-            });
-        }
+    };
+    const constructCmdKey = ({ key: key, metaKey: metaKey, ctrlKey: ctrlKey })=>{
+        let cmd = "";
+        if (metaKey || ctrlKey) cmd += "cmd+";
+        cmd += key;
+        // unify delete
+        cmd = cmd.replace("Del", "del");
+        cmd = cmd.replace("Delete", "del");
+        cmd = cmd.replace("Backspace", "del");
+        // remove "Meta" when cmd only is pressed
+        cmd = cmd.replace("Meta", "");
+        return cmd;
+    };
+    document.addEventListener("keydown", (event)=>{
+        const cmdKey = constructCmdKey(event);
+        const cmd = cmdMap[cmdKey];
+        if (cmd) cmd(event);
     });
     document.addEventListener("click", (event)=>{
         // Close cmd-palette on outside click
@@ -33029,6 +33056,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
             container.classList.remove("block");
         }
     });
+};
+const $915c7ff9e95902f7$var$setupEdgeDrawingHandler = (cy)=>{
     /** Draw edges ============================= */ let tempEdge = null;
     let isDrawing = false;
     let sourceNode = null;
@@ -33124,12 +33153,49 @@ document.addEventListener("DOMContentLoaded", ()=>{
             highlightedNode = null;
         }
     });
-    /** Trigger Optimizat ============================= */ const triggerBtn = document.getElementById("triggerBtn");
+};
+const $915c7ff9e95902f7$var$setUpOptimizer = (cy)=>{
+    /** Trigger Optimization ============================= */ const triggerBtn = document.getElementById("optimizeBtn");
     if (triggerBtn) triggerBtn.onclick = (event)=>{
         cy.layout({
             name: "cola",
             animate: false
         }).run();
+    };
+};
+const $915c7ff9e95902f7$var$setUpAnalyzerr = (cy)=>{
+    /** Trigger Optimization ============================= */ const triggerBtn = document.getElementById("analyzeBtn");
+    // ========= Toggle Metric Containers
+    const degreeCentralityContainer = document.getElementById("degree-centrality");
+    const degreeCentralityBody = degreeCentralityContainer.getElementsByTagName("ul")[0];
+    degreeCentralityContainer.onclick = ()=>{
+        degreeCentralityBody.hidden = !degreeCentralityBody.hidden;
+    };
+    const degreeCentralityNormalizedContainer = document.getElementById("degree-centrality-normalized");
+    const degreeCentralityNormalizedBody = degreeCentralityNormalizedContainer.getElementsByTagName("ul")[0];
+    degreeCentralityNormalizedContainer.onclick = ()=>{
+        degreeCentralityNormalizedBody.hidden = !degreeCentralityNormalizedBody.hidden;
+    };
+    const betweennessCentralityNormalizedContainer = document.getElementById("betweenness-centrality-normalized");
+    const betweennessCentralityNormalizedBody = betweennessCentralityNormalizedContainer.getElementsByTagName("ul")[0];
+    betweennessCentralityNormalizedContainer.onclick = ()=>{
+        betweennessCentralityNormalizedBody.hidden = !betweennessCentralityNormalizedBody.hidden;
+    };
+    const closenessCentralityNormalizedContainer = document.getElementById("closeness-centrality-normalized");
+    const closenessCentralityNormalizedBody = closenessCentralityNormalizedContainer.getElementsByTagName("ul")[0];
+    closenessCentralityNormalizedContainer.onclick = ()=>{
+        closenessCentralityNormalizedBody.hidden = !closenessCentralityNormalizedBody.hidden;
+    };
+    const pageRankContainer = document.getElementById("page-rank");
+    const pageRankBody = pageRankContainer.getElementsByTagName("ul")[0];
+    pageRankContainer.onclick = ()=>{
+        pageRankBody.hidden = !pageRankBody.hidden;
+    };
+    const round = (value, decimals)=>{
+        const shifted = value * Math.pow(10, decimals);
+        return Number((Math.round(shifted) / Math.pow(10, decimals)).toFixed(decimals));
+    };
+    if (triggerBtn) triggerBtn.onclick = ()=>{
         const nodes = cy.nodes();
         const selector = cy.$();
         // ========== Degreee Centrality | Concept: TBD.
@@ -33145,6 +33211,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 in: dc.indegree,
                 out: dc.outdegree
             };
+        }).sort((a, b)=>a.id > b.id ? 1 : -1);
+        degreeCentralityBody.innerHTML = "";
+        degreeCentrality.forEach((item)=>{
+            const li = document.createElement("li"); // Create a new <li> element
+            li.textContent = JSON.stringify(item); // Set its text content
+            degreeCentralityBody.appendChild(li); // Append it to the <ul>
         });
         console.log("========== Degreee Centrality | Concept: Connectivity (In & Out)");
         console.log(degreeCentrality);
@@ -33154,9 +33226,15 @@ document.addEventListener("DOMContentLoaded", ()=>{
             });
             return {
                 id: node.id(),
-                in: dc.indegree(node),
-                out: dc.outdegree(node)
+                in: round(dc.indegree(node), 3),
+                out: round(dc.outdegree(node), 3)
             };
+        }).sort((a, b)=>a.id > b.id ? 1 : -1);
+        degreeCentralityNormalizedBody.innerHTML = "";
+        degreeCentralityNormalized.forEach((item)=>{
+            const li = document.createElement("li"); // Create a new <li> element
+            li.textContent = JSON.stringify(item); // Set its text content
+            degreeCentralityNormalizedBody.appendChild(li); // Append it to the <ul>
         });
         console.log("========== Normalized Degreee Centrality | Concept: Relative connectivity (In & Out).");
         console.log(degreeCentralityNormalized);
@@ -33171,8 +33249,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
        */ const bc = selector.bc();
         const betweenness = nodes.map((node)=>({
                 id: node.id(),
-                bc: bc.betweennessNormalized(node)
+                bc: round(bc.betweennessNormalized(node), 3)
             })).sort((a, b)=>a.bc < b.bc ? 1 : -1);
+        betweennessCentralityNormalizedBody.innerHTML = "";
+        betweenness.forEach((item)=>{
+            const li = document.createElement("li"); // Create a new <li> element
+            li.textContent = JSON.stringify(item); // Set its text content
+            betweennessCentralityNormalizedBody.appendChild(li); // Append it to the <ul>
+        });
         console.log("========== Normalized Betweenness Centrality | Concept: Gatekeepers");
         console.log(betweenness);
         // ========== Normalized Closeness Centrality | Concept: Central / excluded figures
@@ -33184,8 +33268,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
        */ const ccn = selector.ccn();
         const closenessCentrality = nodes.map((node)=>({
                 id: node.id(),
-                cc: ccn.closeness(node)
+                cc: round(ccn.closeness(node), 3)
             })).sort((a, b)=>a.cc < b.cc ? 1 : -1);
+        closenessCentralityNormalizedBody.innerHTML = "";
+        closenessCentrality.forEach((item)=>{
+            const li = document.createElement("li"); // Create a new <li> element
+            li.textContent = JSON.stringify(item); // Set its text content
+            closenessCentralityNormalizedBody.appendChild(li); // Append it to the <ul>
+        });
         console.log("========== Normalized Closeness Centrality | Concept: Central / excluded figures");
         console.log(closenessCentrality);
         // ========== Normalized Page Rank | Concept: Relative Importance
@@ -33195,25 +33285,29 @@ document.addEventListener("DOMContentLoaded", ()=>{
        */ const pr = cy.elements().pageRank();
         const pageRanks = nodes.map((node)=>({
                 id: node.id(),
-                pr: pr.rank(node)
+                pr: round(pr.rank(node), 3)
             })).sort((a, b)=>a.pr < b.pr ? 1 : -1);
+        pageRankBody.innerHTML = "";
+        pageRanks.forEach((item)=>{
+            const li = document.createElement("li"); // Create a new <li> element
+            li.textContent = JSON.stringify(item); // Set its text content
+            pageRankBody.appendChild(li); // Append it to the <ul>
+        });
         console.log("========== Page Rank | Concept: TBD");
         console.log(pageRanks);
+        cy.resize();
     };
-    /** SAVING ====================================  */ function saveGraph() {
-        console.log("saved");
-        const elements = cy.json().elements; // Get the current state of the graph
-        localStorage.setItem("cyGraph", JSON.stringify(elements)); // Save it as a string
-    }
-    function loadGraph() {
-        const savedData = localStorage.getItem("cyGraph");
-        if (savedData) {
-            const json = JSON.parse(savedData); // Parse it back to JSON
-            return Object.keys(json).length ? json : null;
-        }
-        return null; // or return a default state if nothing is saved
-    }
-});
+};
+const $915c7ff9e95902f7$var$saveGraph = (elements)=>{
+    localStorage.setItem("cyGraph", JSON.stringify(elements));
+    console.log("Succesfully saved graph!");
+};
+const $915c7ff9e95902f7$var$loadGraph = ()=>{
+    const savedData = localStorage.getItem("cyGraph");
+    if (!savedData) return null;
+    const json = JSON.parse(savedData);
+    return Object.keys(json).length ? json : null;
+};
 
 
-//# sourceMappingURL=index.318d529d.js.map
+//# sourceMappingURL=index.f6653313.js.map
