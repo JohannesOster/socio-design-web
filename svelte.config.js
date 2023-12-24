287bcf0900f3f38e210221c1b26573966b9e03da
Johannes Oster <johannesoster@192.168.68.104>
2023-12-24 18:40:11 +0100

    Remove base path in svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess({ postcss: true }),
-	kit: { adapter: adapter(), paths: { base: '/socio-design-web' } }
+	kit: { adapter: adapter(), appDir: 'app' }
 };
 
 export default config;
