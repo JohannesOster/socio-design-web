10f401dd715bcac14e9533937d70c7a1736287fb
Johannes Oster <johannesoster@192.168.68.104>
2023-12-25 23:49:40 +0100

    Add proper chevron animation for sidebar

--- src/lib/components/SideBar/SideBar.svelte
+++ src/lib/components/SideBar/SideBar.svelte
@@ -122,9 +122,24 @@
 </script>
 
 <aside class="sidebar" class:open={!isSidebarOpen}>
-	<button class="toggleBtn" on:click={() => (isSidebarOpen = !isSidebarOpen)}
-		>{isSidebarOpen ? '<' : '>'}</button
-	>
+	<button class="toggle-btn" on:click={() => (isSidebarOpen = !isSidebarOpen)}>
+		<svg
+			height="16px"
+			width="16px"
+			viewBox="0 0 185.343 185.343"
+			class:rotate-y={isSidebarOpen}
+			class="transition-[transform] transform ease-in-out duration-500 sm:duration-300"
+		>
+			<g>
+				<path
+					fill="current-color"
+					d="M51.707,185.343c-2.741,0-5.493-1.044-7.593-3.149c-4.194-4.194-4.194-10.981,0-15.175
+			l74.352-74.347L44.114,18.32c-4.194-4.194-4.194-10.987,0-15.175c4.194-4.194,10.987-4.194,15.18,0l81.934,81.934
+			c4.194,4.194,4.194,10.987,0,15.175l-81.934,81.939C57.201,184.293,54.454,185.343,51.707,185.343z"
+				/>
+			</g>
+		</svg>
+	</button>
 
 	<div class="flex flex-col p-4 pr-0 gap-4 h-full">
 		<div class="flex flex-col bg-white rounded-md">
@@ -164,12 +179,16 @@
 		@apply pointer-events-auto;
 	}
 
-	.toggleBtn {
-		@apply absolute top-4 right-0 bg-white px-4 py-2 rounded-full;
+	.toggle-btn {
+		@apply absolute top-4 right-0 bg-white p-3 rounded-full;
 		transform: translateX(calc(100% + 16px));
 	}
 
 	.open {
 		@apply -translate-x-full;
+	}
+
+	.rotate-y {
+		transform: rotateY(180deg);
 	}
 </style>
